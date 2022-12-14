0370eee96f4b41c6313c2a8247c6b6b3472e42c4
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-14 20:31:24 -0300

    Init Project

---
+++ ChessGameSystem/ChessGameSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChessGameSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(3, 4);
+        }
+    }
+}
