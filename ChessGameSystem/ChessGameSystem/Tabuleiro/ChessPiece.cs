83bea0d69c4b985d82684d6fe7b8dd3a52b6db26
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-14 21:03:39 -0300

    Create ChessPiece.cs

---
+++ ChessGameSystem/ChessGameSystem/Tabuleiro/ChessPiece.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameSystem.Tabuleiro
+{
+    public class ChessPiece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; set; }
+        public int AmountMoves { get; set; }
+        public ChessBoard ChessBoard { get; set; }
+
+        public ChessPiece(Position position, Color color, ChessBoard chessBoard)
+        {
+            Position = position;
+            Color = color;
+            ChessBoard = chessBoard;
+        }
+    }
+}
