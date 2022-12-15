d405d2d3e4deb35e8f5ed66fe494ed4e490955fd
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-14 21:03:44 -0300

    Create ChessBoard.cs

---
+++ ChessGameSystem/ChessGameSystem/Tabuleiro/ChessBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameSystem.Tabuleiro
+{
+    public class ChessBoard // not inherit from position because the objective is another
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+        private ChessPiece[,] Pieces { get; set; }
+
+        public ChessBoard(int lines, int columns)
+        {
+            Lines = lines;
+            Columns = columns;
+            Pieces = new ChessPiece[lines, columns];
+        }
+    }
+}
