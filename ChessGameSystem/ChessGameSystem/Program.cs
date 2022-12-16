0ae268103c3c7353e1882d76cbcdd6353587ab2b
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-16 12:16:37 -0300

    Testing colors in chessBoard

--- ChessGameSystem/ChessGameSystem/Program.cs
+++ ChessGameSystem/ChessGameSystem/Program.cs
@@ -13,7 +13,11 @@ namespace ChessGameSystem
                 ChessBoard chessBoard = new ChessBoard(8, 8);
                 chessBoard.PutPiece(new Tower(Color.Black, chessBoard), new Position(0, 0));
                 chessBoard.PutPiece(new Tower(Color.Black, chessBoard), new Position(1, 3));
-                chessBoard.PutPiece(new King(Color.Black, chessBoard), new Position(0, 0));
+                chessBoard.PutPiece(new King(Color.Black, chessBoard), new Position(2, 0));
+
+                chessBoard.PutPiece(new Tower(Color.White, chessBoard), new Position(2, 1));
+                chessBoard.PutPiece(new Tower(Color.White, chessBoard), new Position(3, 2));
+                chessBoard.PutPiece(new King(Color.White, chessBoard), new Position(2, 5));
 
                 Screen.ShowChessBoard(chessBoard);
             }
