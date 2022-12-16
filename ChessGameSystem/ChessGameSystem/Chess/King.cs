16116167afa91a384beff4401ff17330410a9508
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-16 10:54:01 -0300

    Create (King and Tower)Piece

---
+++ ChessGameSystem/ChessGameSystem/Chess/King.cs
@@ -0,0 +1,19 @@
+﻿using ChessGameSystem.Tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameSystem.Chess
+{
+    public class King : ChessPiece
+    {
+        public King(Color color, ChessBoard chessBoard) : base(color, chessBoard) { }
+
+        public override string ToString()
+        {
+            return "K";
+        }
+    }
+}
---
+++ ChessGameSystem/ChessGameSystem/Chess/Tower.cs
@@ -0,0 +1,19 @@
+﻿using ChessGameSystem.Tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameSystem.Chess
+{
+    public class Tower : ChessPiece
+    {
+        public Tower(Color color, ChessBoard chessBoard) : base(color, chessBoard) { }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
