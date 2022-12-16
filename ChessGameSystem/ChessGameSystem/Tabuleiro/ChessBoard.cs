66a5560ed874276700c56bf83854362e0384c754
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-16 11:37:52 -0300

    Create validates to position
    Create validate to the position of the piece and the position of the board

--- ChessGameSystem/ChessGameSystem/Tabuleiro/ChessBoard.cs
+++ ChessGameSystem/ChessGameSystem/Tabuleiro/ChessBoard.cs
@@ -19,6 +19,11 @@ namespace ChessGameSystem.Tabuleiro
             Pieces = new ChessPiece[lines, columns];
         }
 
+        public ChessPiece ChessPiece(Position position)
+        {
+            return Pieces[position.Line, position.Column];
+        }
+
         public ChessPiece ChessPiece(int line, int column)
         {
             return Pieces[line, column];
@@ -26,8 +31,29 @@ namespace ChessGameSystem.Tabuleiro
 
         public void PutPiece(ChessPiece piece, Position position)
         {
+            if (ExistsPiece(position)) 
+                throw new Exception("There is a piece this position");
+
             Pieces[position.Line, position.Column] = piece;
-            piece.Position = position; 
+            piece.Position = position;
+        }
+
+        //TODO 1 : This function can be transferred to ValidatePosition's "IF" command, if you have no more references.
+        public bool ExistsPosition(Position position)
+        {
+            return !(position.Line < 0 || position.Line >= Lines || position.Column < 0 || position.Column >= Columns);
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!ExistsPosition(pos))
+                throw new ArgumentException("Position invalidate");
+        }
+
+        public bool ExistsPiece(Position pos)
+        {
+            ValidatePosition(pos);
+            return Pieces[pos.Line, pos.Column] != null;
         }
     }
 }
