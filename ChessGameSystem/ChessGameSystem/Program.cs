ad66e9d9961e421a2661e90b4b6403fd740cd955
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-16 11:38:36 -0300

    Testing exceptions

--- ChessGameSystem/ChessGameSystem/Program.cs
+++ ChessGameSystem/ChessGameSystem/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChessGameSystem.Chess;
+using ChessGameSystem.Tabuleiro;
+using System;
 
 namespace ChessGameSystem
 {
@@ -6,7 +8,17 @@ namespace ChessGameSystem
     {
         static void Main(string[] args)
         {
-            Position p = new Position(3, 4);
+            try
+            {
+                ChessBoard chessBoard = new ChessBoard(8, 8);
+                chessBoard.PutPiece(new Tower(Color.Black, chessBoard), new Position(0, 0));
+                chessBoard.PutPiece(new Tower(Color.Black, chessBoard), new Position(1, 3));
+                chessBoard.PutPiece(new King(Color.Black, chessBoard), new Position(0, 0));
+
+                Screen.ShowChessBoard(chessBoard);
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message); }
         }
     }
 }
