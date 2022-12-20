1bfd45cb977052805a251c8025019ba8930dddab
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-20 14:54:19 -0300

    IncriminateMovements
    IncriminateMovements

--- ChessGameSystem/ChessGameSystem/Tabuleiro/ChessPiece.cs
+++ ChessGameSystem/ChessGameSystem/Tabuleiro/ChessPiece.cs
@@ -4,13 +4,18 @@
     {
         public Position Position { get; set; }
         public Color Color { get; set; }
-        public int AmountMoves { get; set; }
+        private int AmountMoves { get; set; }
         public ChessBoard ChessBoard { get; set; }
 
         public ChessPiece(Color color, ChessBoard chessBoard)
         {
             Color = color;
             ChessBoard = chessBoard;
+        }
+
+        public void IncriminateMovements()
+        {
+            AmountMoves++;
         }
     }
 }
