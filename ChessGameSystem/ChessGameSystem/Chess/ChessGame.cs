7f1a25bdb68ba7cad2d135d9f17bf632ea3cee89
Gustavo Luiz <glogustavo4321@gmail.com>
2022-12-20 14:56:10 -0300

    Create ChessGame.cs
    Create ChessGame with executeMovement and testing PUTAllPieces in chessboard

---
+++ ChessGameSystem/ChessGameSystem/Chess/ChessGame.cs
@@ -0,0 +1,52 @@
+﻿using ChessGameSystem.Tabuleiro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameSystem.Chess
+{
+    public class ChessGame
+    {
+        public ChessBoard ChessBoard { get;}
+        private int Turno { get; set; }
+        private Color CurrentPlayer { get; set; }
+        public bool Finished { get; }
+
+        public ChessGame()
+        {
+            ChessBoard = new ChessBoard(8, 8);
+            Turno = 1;
+            CurrentPlayer = Color.White;
+            PutAllPieces();
+        }
+
+        public void ExecuteMovement(Position origin, Position destination)
+        {
+            ChessPiece piece = ChessBoard.RemovePiece(origin);
+            piece.IncriminateMovements();
+            
+            ChessPiece capturedPiece= ChessBoard.RemovePiece(destination);
+            ChessBoard.PutPiece(piece, destination);
+        }
+
+        private void PutAllPieces()
+        {
+            ChessBoard.PutPiece(new Tower(Color.White, ChessBoard), new PositionChess('c', 1).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.White, ChessBoard), new PositionChess('c', 2).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.White, ChessBoard), new PositionChess('d', 2).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.White, ChessBoard), new PositionChess('e', 2).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.White, ChessBoard), new PositionChess('e', 1).ToPosition());
+            ChessBoard.PutPiece(new King(Color.White, ChessBoard), new PositionChess('d', 1).ToPosition());
+
+            ChessBoard.PutPiece(new Tower(Color.Black, ChessBoard), new PositionChess('c', 7).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.Black, ChessBoard), new PositionChess('c', 8).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.Black, ChessBoard), new PositionChess('d', 7).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.Black, ChessBoard), new PositionChess('e', 7).ToPosition());
+            ChessBoard.PutPiece(new Tower(Color.Black, ChessBoard), new PositionChess('e', 8).ToPosition());
+            ChessBoard.PutPiece(new King(Color.Black, ChessBoard), new PositionChess('d', 8).ToPosition());
+        }
+
+    }
+}
